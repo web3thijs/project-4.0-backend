0e0cdec7606d0edab8621171dbeea3740e19d63b
Gianni De Herdt <57660947+r0790225@users.noreply.github.com>
2022-01-18 10:47:01 +0100

    gdh-addProductModel

--- src/main/java/fact/it/backend/model/Product.java
+++ src/main/java/fact/it/backend/model/Product.java
@@ -1,4 +1,117 @@
 package fact.it.backend.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
 public class Product {
+
+    @Id
+    private String id;
+    private String categoryId;
+    private String organizationId;
+    private String name;
+    private int price;
+    private Boolean isActive;
+    private int amountInStock;
+    private String size;
+    private String color;
+    private String imageUrl;
+
+    public Product(){
+
+    }
+
+    public Product(String id, String categoryId, String organizationId, String name, int price, Boolean isActive, int amountInStock, String size, String color, String imageUrl) {
+        this.id = id;
+        this.categoryId = categoryId;
+        this.organizationId = organizationId;
+        this.name = name;
+        this.price = price;
+        this.isActive = isActive;
+        this.amountInStock = amountInStock;
+        this.size = size;
+        this.color = color;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(String organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    public int getAmountInStock() {
+        return amountInStock;
+    }
+
+    public void setAmountInStock(int amountInStock) {
+        this.amountInStock = amountInStock;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
