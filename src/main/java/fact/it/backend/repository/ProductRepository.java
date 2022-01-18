9d0a2ce03f12f5fd7befc73cadf980caab914df9
Gianni De Herdt <57660947+r0790225@users.noreply.github.com>
2022-01-18 11:01:24 +0100

    gdh-addProductRepository

--- src/main/java/fact/it/backend/repository/ProductRepository.java
+++ src/main/java/fact/it/backend/repository/ProductRepository.java
@@ -1,4 +1,14 @@
 package fact.it.backend.repository;
 
-public interface ProductRepository {
+import fact.it.backend.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+    List<Product> findAllProducts();
+    List<Product> findProductsByOrganization(String organizationId);
+    Product findProductById(String id);
 }
