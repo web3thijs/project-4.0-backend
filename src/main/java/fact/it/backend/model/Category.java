676c0d1bcdaed906b9e02af5bea61d7461f9ae65
Thijs <r0793167@student.thomasmore.be>
2022-01-19 09:30:24 +0100

    Category Model

--- src/main/java/fact/it/backend/model/Category.java
+++ src/main/java/fact/it/backend/model/Category.java
@@ -1,4 +1,37 @@
 package fact.it.backend.model;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "categories")
 public class Category {
+
+    @Id
+    private ObjectId id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(ObjectId id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
