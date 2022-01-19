5d8e3f2b72083a723dc98b5533a932854fe2d6ed
Gianni De Herdt <57660947+r0790225@users.noreply.github.com>
2022-01-19 09:55:27 +0100

    gdh-addStockRoute

--- src/main/java/fact/it/backend/controller/StockController.java
+++ src/main/java/fact/it/backend/controller/StockController.java
@@ -1,4 +1,74 @@
 package fact.it.backend.controller;
 
+import fact.it.backend.model.Product;
+import fact.it.backend.model.Stock;
+import fact.it.backend.repository.StockRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@RequestMapping(path = "api/stocks")
+@RestController
 public class StockController {
+
+    @Autowired
+    StockRepository stockRepository;
+
+    @PostConstruct
+    public void fillDB(){
+        if(stockRepository.count() == 0){
+            ObjectId id1 = new ObjectId();
+            ObjectId id2 = new ObjectId();
+            stockRepository.save(new Stock(id1, id1, id1, id1, 10));
+            stockRepository.save(new Stock(id2, id2, id2, id2, 15));
+        }
+        System.out.println("DB test stocks: " + stockRepository.findAll().size() + " stocks.");
+
+    }
+
+    @GetMapping("")
+    public List<Stock> findAll(){
+        return stockRepository.findAll();
+    }
+
+    @GetMapping("/product/{productId}")
+    public List<Stock> findStocksByProductId(@PathVariable ObjectId productId){
+        return stockRepository.findStocksByProductId(productId);
+    }
+
+    @PostMapping("")
+    public Stock addStock(@RequestBody Stock stock){
+        stockRepository.save(stock);
+        return stock;
+    }
+
+    @PutMapping("")
+    public Stock updateStock(@RequestBody Stock updatedStock){
+        Stock retrievedStock = stockRepository.findStockById(updatedStock.getId());
+
+        retrievedStock.setSizeId(updatedStock.getSizeId());
+        retrievedStock.setColorId(updatedStock.getColorId());
+        retrievedStock.setProductId(updatedStock.getProductId());
+        retrievedStock.setAmountInStock(updatedStock.getAmountInStock());
+
+        stockRepository.save(retrievedStock);
+
+        return retrievedStock;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteStock(@PathVariable ObjectId id){
+        Stock stock = stockRepository.findStockById(id);
+
+        if(stock != null){
+            stockRepository.delete(stock);
+            return ResponseEntity.ok().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- src/main/java/fact/it/backend/model/Stock.java
+++ src/main/java/fact/it/backend/model/Stock.java
@@ -1,4 +1,67 @@
 package fact.it.backend.model;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "stocks")
 public class Stock {
+
+    @Id
+    private ObjectId id;
+    private ObjectId sizeId;
+    private ObjectId colorId;
+    private ObjectId productId;
+    private int amountInStock;
+
+    public Stock() {
+    }
+
+    public Stock(ObjectId id, ObjectId sizeId, ObjectId colorId, ObjectId productId, int amountInStock) {
+        this.id = id;
+        this.sizeId = sizeId;
+        this.colorId = colorId;
+        this.productId = productId;
+        this.amountInStock = amountInStock;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public ObjectId getSizeId() {
+        return sizeId;
+    }
+
+    public void setSizeId(ObjectId sizeId) {
+        this.sizeId = sizeId;
+    }
+
+    public ObjectId getColorId() {
+        return colorId;
+    }
+
+    public void setColorId(ObjectId colorId) {
+        this.colorId = colorId;
+    }
+
+    public ObjectId getProductId() {
+        return productId;
+    }
+
+    public void setProductId(ObjectId productId) {
+        this.productId = productId;
+    }
+
+    public int getAmountInStock() {
+        return amountInStock;
+    }
+
+    public void setAmountInStock(int amountInStock) {
+        this.amountInStock = amountInStock;
+    }
 }
