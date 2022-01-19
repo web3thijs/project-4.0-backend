1cd52a7a4effdaf1d2d114de3404cab8096f815d
Gianni De Herdt <57660947+r0790225@users.noreply.github.com>
2022-01-19 12:57:45 +0100

    gdh-addInteractionRepository

--- src/main/java/fact/it/backend/repository/InteractionRepository.java
+++ src/main/java/fact/it/backend/repository/InteractionRepository.java
@@ -1,4 +1,16 @@
 package fact.it.backend.repository;
 
-public interface InteractionRepository {
+import fact.it.backend.model.Interaction;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InteractionRepository extends MongoRepository<Interaction, String> {
+    List<Interaction> findAll();
+    List<Interaction> findInteractionsByProductId(String productId);
+    List<Interaction> findInteractionsByCustomerId(String customerId);
+    Interaction findInteractionById(String id);
 }
+
