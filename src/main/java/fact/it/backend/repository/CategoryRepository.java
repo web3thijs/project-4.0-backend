c490bc6be262a8c31ab57cb96036ad9cdea5c5ba
Thijs <r0793167@student.thomasmore.be>
2022-01-19 09:47:06 +0100

    Category Repository

--- src/main/java/fact/it/backend/repository/CategoryRepository.java
+++ src/main/java/fact/it/backend/repository/CategoryRepository.java
@@ -1,4 +1,14 @@
 package fact.it.backend.repository;
 
-public interface CategoryRepository {
+import fact.it.backend.model.Category;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoryRepository extends MongoRepository<Category, ObjectId> {
+    List<Category> findAll();
+    Category findByCategoryId(ObjectId id);
 }
--- src/main/java/fact/it/backend/repository/ProductRepository.java
+++ src/main/java/fact/it/backend/repository/ProductRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends MongoRepository<Product, String> {
+public interface ProductRepository extends MongoRepository<Product, ObjectId> {
     List<Product> findAll();
     List<Product> findProductsByOrganizationId(ObjectId organizationId);
     Product findProductById(ObjectId id);
