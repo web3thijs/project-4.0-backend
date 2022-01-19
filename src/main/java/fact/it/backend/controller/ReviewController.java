5076e280744ae998ab500139d0af081d098bcf4b
Gianni De Herdt <57660947+r0790225@users.noreply.github.com>
2022-01-19 11:15:23 +0100

    gdh-addReview

--- src/main/java/fact/it/backend/controller/ReviewController.java
+++ src/main/java/fact/it/backend/controller/ReviewController.java
@@ -1,4 +1,31 @@
 package fact.it.backend.controller;
 
+import fact.it.backend.model.Review;
+import fact.it.backend.repository.ReviewRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@RequestMapping(path = "api/reviews")
+@RestController
 public class ReviewController {
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    @PostConstruct
+    public void fillDB(){
+        if(reviewRepository.count() == 0){
+            String customer1 = new ObjectId().toString();
+            String customer2 = new ObjectId().toString();
+            String product1 = new ObjectId().toString();
+            String product2 = new ObjectId().toString();
+            reviewRepository.save(new Review(product1, customer1, 4, "Good", ""));
+            reviewRepository.save(new Review(product2, customer1, 4.5, "Nice product", "I liked it."));
+            reviewRepository.save(new Review(product1, customer2, 3, "Nice product", "I liked it."));
+        }
+    }
 }
--- src/main/java/fact/it/backend/repository/ReviewRepository.java
+++ src/main/java/fact/it/backend/repository/ReviewRepository.java
@@ -1,4 +1,15 @@
 package fact.it.backend.repository;
 
-public interface ReviewRepository {
+import fact.it.backend.model.Review;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, String> {
+    List<Review> findAll();
+    List<Review> findReviewsByProductId(String productId);
+    List<Review> findReviewsByCustomerId(String customerId);
+    Review findReviewById(String id);
 }
