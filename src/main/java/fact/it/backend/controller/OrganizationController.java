f497a6f8fc06a3afa495f1965f336b194aa5fccd
Thijs <r0793167@student.thomasmore.be>
2022-01-20 09:17:08 +0100

    Organizations CRUD

--- src/main/java/fact/it/backend/controller/OrganizationController.java
+++ src/main/java/fact/it/backend/controller/OrganizationController.java
@@ -3,10 +3,8 @@ package fact.it.backend.controller;
 import fact.it.backend.model.*;
 import fact.it.backend.repository.OrganizationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -36,5 +34,46 @@ public class OrganizationController {
     @GetMapping("/{id}")
     public Organization findById(@PathVariable String id){
         return organizationRepository.findByRoleAndId(Role.ORGANIZATION, id);
+    }
+
+    @PostMapping("")
+    public Organization addOrganization(@RequestBody Organization organization) {
+        organizationRepository.save(organization);
+        return organization;
+    }
+
+    @PutMapping
+    public Organization updateOrganization(@RequestBody Organization updatedOrganizationr){
+        Organization retrievedOrganization= organizationRepository.findByRoleAndId(Role.ORGANIZATION, updatedOrganizationr.getId());
+
+        retrievedOrganization.setEmail(updatedOrganizationr.getEmail());
+        retrievedOrganization.setPassword(updatedOrganizationr.getPassword());
+        retrievedOrganization.setPhoneNr(updatedOrganizationr.getPhoneNr());
+        retrievedOrganization.setAddress(updatedOrganizationr.getAddress());
+        retrievedOrganization.setPostalCode(updatedOrganizationr.getPostalCode());
+        retrievedOrganization.setCountry(updatedOrganizationr.getCountry());
+        retrievedOrganization.setRole(updatedOrganizationr.getRole());
+        retrievedOrganization.setOrganizationName(updatedOrganizationr.getOrganizationName());
+        retrievedOrganization.setCompanyRegistrationNr(updatedOrganizationr.getCompanyRegistrationNr());
+        retrievedOrganization.setVatNr(updatedOrganizationr.getVatNr());
+        retrievedOrganization.setAbout(updatedOrganizationr.getAbout());
+        retrievedOrganization.setSupportPhoneNr(updatedOrganizationr.getSupportPhoneNr());
+        retrievedOrganization.setSupportEmail(updatedOrganizationr.getSupportEmail());
+
+        organizationRepository.save(retrievedOrganization);
+
+        return retrievedOrganization;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteOrganization(@PathVariable String id){
+        Organization organization = organizationRepository.findByRoleAndId(Role.ORGANIZATION, id);
+
+        if(organization != null){
+            organizationRepository.delete(organization);
+            return ResponseEntity.ok().build();
+        } else{
+            return ResponseEntity.notFound().build();
+        }
     }
 }
