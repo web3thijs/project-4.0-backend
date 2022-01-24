78e03e9540ddf69b4cc0240b64d153df948fa0c3
Gianni De Herdt <57660947+r0790225@users.noreply.github.com>
2022-01-24 10:01:48 +0100

    gdh-DonationModel

--- src/main/java/fact/it/backend/model/Donation.java
+++ src/main/java/fact/it/backend/model/Donation.java
@@ -1,4 +1,51 @@
 package fact.it.backend.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "donations")
 public class Donation {
+
+    @Id
+    private String id;
+    private Product product;
+    private Organization organization;
+    private Number amount;
+
+    public Donation() {
+    }
+
+    public Donation(Product product, Organization organization, Number amount) {
+        this.product = product;
+        this.organization = organization;
+        this.amount = amount;
+    }
+
+    public String getId() {
+        return id;
+    }
+    
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Organization getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(Organization organization) {
+        this.organization = organization;
+    }
+
+    public Number getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Number amount) {
+        this.amount = amount;
+    }
 }
