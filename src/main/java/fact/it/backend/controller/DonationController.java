f03968d47d0c22ae392e54ea555ae36cc8de97f0
Gianni De Herdt <57660947+r0790225@users.noreply.github.com>
2022-01-24 09:58:28 +0100

    gdh-DonationRepository

--- src/main/java/fact/it/backend/controller/DonationController.java
+++ src/main/java/fact/it/backend/controller/DonationController.java
@@ -1,9 +1,18 @@
 package fact.it.backend.controller;
 
+import fact.it.backend.model.Donation;
+import fact.it.backend.repository.DonationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(path = "api/donations")
 @RestController
 public class DonationController {
+    @Autowired
+    DonationRepository donationRepository;
+
+    @GetMapping("")
+    public List<Donation> findAll() { return donationRepository.findAll();}
 }
--- src/main/java/fact/it/backend/repository/DonationRepository.java
+++ src/main/java/fact/it/backend/repository/DonationRepository.java
@@ -1,4 +1,13 @@
 package fact.it.backend.repository;
 
-public interface DonationRepository {
+import fact.it.backend.model.Donation;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DonationRepository extends MongoRepository<Donation, String> {
+    List<Donation> findAll();
+    List<Donation> findDonationById(String id);
 }
