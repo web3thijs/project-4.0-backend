a9c2528c885406eabcaa00a89ad773a0cf9ee07f
Gianni De Herdt <57660947+r0790225@users.noreply.github.com>
2022-01-25 14:47:21 +0100

    gdh-OrderSorting

--- src/main/java/fact/it/backend/controller/OrderController.java
+++ src/main/java/fact/it/backend/controller/OrderController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -34,15 +35,28 @@ public class OrderController {
 
 
     @GetMapping("")
-    public ResponseEntity<?> findAll(@RequestHeader("Authorization") String tokenWithPrefix, @RequestParam int page){
-        Pageable requestedPage = PageRequest.of(page, 8);
-        Page<Order> orders = orderRepository.findAll(requestedPage);
+    public ResponseEntity<?> findAll(@RequestHeader("Authorization") String tokenWithPrefix, @RequestParam int page, @RequestParam(required = false) String sort, @RequestParam(required = false)String order){
         String token = tokenWithPrefix.substring(7);
         Map<String, Object> claims = jwtUtils.extractAllClaims(token);
         String role = claims.get("role").toString();
 
         if(role.contains("ADMIN")){
-            return ResponseEntity.ok(orders);
+            if(sort != null){
+                if(order != null){
+                    Pageable requestedPageWithSortDesc = PageRequest.of(page, 8, Sort.by(sort).descending());
+                    Page<Order> orders = orderRepository.findAll(requestedPageWithSortDesc);
+                    return ResponseEntity.ok(orders);
+                }
+                else{
+                    Pageable requestedPageWithSort = PageRequest.of(page, 8, Sort.by(sort).ascending());
+                    Page<Order> orders = orderRepository.findAll(requestedPageWithSort);
+                    return ResponseEntity.ok(orders);
+                }
+            }else{
+                Pageable requestedPage = PageRequest.of(page, 8, Sort.by("name").ascending());
+                Page<Order> orders = orderRepository.findAll(requestedPage);
+                return ResponseEntity.ok(orders);
+            }
         } else {
             return new ResponseEntity<String>("Forbidden", HttpStatus.FORBIDDEN);
         }
@@ -64,16 +78,29 @@ public class OrderController {
     }
 
     @GetMapping("/customer/{customerId}")
-    public ResponseEntity<?> findOrdersByCustomerId(@RequestHeader("Authorization") String tokenWithPrefix, @PathVariable String customerId, @RequestParam int page){
-        Pageable requestedPage = PageRequest.of(page, 8);
-        Page<Order> orders = orderRepository.findOrdersByCustomerId(customerId, requestedPage);
+    public ResponseEntity<?> findOrdersByCustomerId(@RequestHeader("Authorization") String tokenWithPrefix, @PathVariable String customerId, @RequestParam int page, @RequestParam(required = false) String sort, @RequestParam(required = false)String order){
         String token = tokenWithPrefix.substring(7);
         Map<String, Object> claims = jwtUtils.extractAllClaims(token);
         String role = claims.get("role").toString();
         String user_id = claims.get("user_id").toString();
 
         if(role.contains("ADMIN") || (role.contains("CUSTOMER") && customerId.contains(user_id))){
-            return ResponseEntity.ok(orders);
+            if(sort != null){
+                if(order != null){
+                    Pageable requestedPageWithSortDesc = PageRequest.of(page, 8, Sort.by(sort).descending());
+                    Page<Order> orders = orderRepository.findOrdersByCustomerId(customerId, requestedPageWithSortDesc);
+                    return ResponseEntity.ok(orders);
+                }
+                else{
+                    Pageable requestedPageWithSort = PageRequest.of(page, 8, Sort.by(sort).ascending());
+                    Page<Order> orders = orderRepository.findOrdersByCustomerId(customerId, requestedPageWithSort);
+                    return ResponseEntity.ok(orders);
+                }
+            }else{
+                Pageable requestedPage = PageRequest.of(page, 8, Sort.by("name").ascending());
+                Page<Order> orders = orderRepository.findOrdersByCustomerId(customerId, requestedPage);
+                return ResponseEntity.ok(orders);
+            }
         } else {
             return new ResponseEntity<String>("Forbidden", HttpStatus.FORBIDDEN);
         }
