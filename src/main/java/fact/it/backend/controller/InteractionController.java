f9016bd9ac733179b6403897af4c7afbec95e70d
Gianni De Herdt <57660947+r0790225@users.noreply.github.com>
2022-02-08 01:58:59 +0100

    gdh-OrderValidation

--- src/main/java/fact/it/backend/controller/InteractionController.java
+++ src/main/java/fact/it/backend/controller/InteractionController.java
@@ -1,6 +1,7 @@
 package fact.it.backend.controller;
 
 import fact.it.backend.dto.AddToInteractionDTO;
+import fact.it.backend.exception.ResourceNotFoundException;
 import fact.it.backend.model.*;
 import fact.it.backend.repository.CustomerRepository;
 import fact.it.backend.repository.InteractionRepository;
@@ -13,7 +14,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "api/interactions")
@@ -49,38 +52,50 @@ public class InteractionController {
         if(role.contains("ADMIN")){
             return ResponseEntity.ok(interactionRepository.findAll());
         } else {
-            return new ResponseEntity<String>("Forbidden", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<String>("Not authorized", HttpStatus.FORBIDDEN);
         }
     }
 
     @GetMapping("/product/{productId}")
-    public ResponseEntity<?> findInteractionsByProductId(@RequestHeader("Authorization") String tokenWithPrefix, @PathVariable long productId){
+    public ResponseEntity<?> findInteractionsByProductId(@RequestHeader("Authorization") String tokenWithPrefix, @PathVariable long productId) throws ResourceNotFoundException {
         String token = tokenWithPrefix.substring(7);
         Map<String, Object> claims = jwtUtils.extractAllClaims(token);
         String role = claims.get("role").toString();
 
         if(role.contains("ADMIN")){
-            return ResponseEntity.ok(interactionRepository.findInteractionsByProductId(productId));
+            Product product = productRepository.findProductById(productId);
+            if(product.getInteractions().size() != 0){
+                return ResponseEntity.ok(interactionRepository.findInteractionsByProductId(productId));
+            }
+            else{
+                throw new ResourceNotFoundException("No interactions found for product with id: " + productId);
+            }
         } else {
-            return new ResponseEntity<String>("Forbidden", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<String>("Not authorized", HttpStatus.FORBIDDEN);
         }
     }
 
     @GetMapping("/customer/{customerId}")
-    public ResponseEntity<?> findInteractionsByCustomerId(@RequestHeader("Authorization") String tokenWithPrefix, @PathVariable long customerId){
+    public ResponseEntity<?> findInteractionsByCustomerId(@RequestHeader("Authorization") String tokenWithPrefix, @PathVariable long customerId) throws ResourceNotFoundException {
         String token = tokenWithPrefix.substring(7);
         Map<String, Object> claims = jwtUtils.extractAllClaims(token);
         String role = claims.get("role").toString();
 
         if(role.contains("ADMIN")){
-            return ResponseEntity.ok(interactionRepository.findInteractionsByCustomerId(customerId));
+            Customer customer = customerRepository.findCustomerById(customerId);
+            if(customer.getInteractions().size() != 0){
+                return ResponseEntity.ok(interactionRepository.findInteractionsByCustomerId(customerId));
+            }
+            else{
+                throw new ResourceNotFoundException("No interactions found for customer with id: " + customerId);
+            }
         } else {
-            return new ResponseEntity<String>("Forbidden", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<String>("Not authorized", HttpStatus.FORBIDDEN);
         }
     }
 
     @PostMapping
-    public ResponseEntity<?> addInteraction(@RequestHeader("Authorization") String tokenWithPrefix, @RequestBody Interaction interaction){
+    public ResponseEntity<?> addInteraction(@RequestHeader("Authorization") String tokenWithPrefix, @Valid @RequestBody Interaction interaction){
         String token = tokenWithPrefix.substring(7);
         Map<String, Object> claims = jwtUtils.extractAllClaims(token);
         String role = claims.get("role").toString();
@@ -89,36 +104,41 @@ public class InteractionController {
             interactionRepository.save(interaction);
             return ResponseEntity.ok(interaction);
         } else {
-            return new ResponseEntity<String>("Forbidden", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<String>("Not authorized", HttpStatus.FORBIDDEN);
         }
     }
 
 
 
     @PutMapping
-    public ResponseEntity<?> updateInteraction(@RequestHeader("Authorization") String tokenWithPrefix, @RequestBody Interaction updatedInteraction){
+    public ResponseEntity<?> updateInteraction(@RequestHeader("Authorization") String tokenWithPrefix, @Valid @RequestBody Interaction updatedInteraction) throws ResourceNotFoundException {
         String token = tokenWithPrefix.substring(7);
         Map<String, Object> claims = jwtUtils.extractAllClaims(token);
         String role = claims.get("role").toString();
         long user_id = Long.parseLong(claims.get("user_id").toString());
         Interaction retrievedInteraction = interactionRepository.findInteractionById(updatedInteraction.getId());
 
-        if(role.contains("ADMIN") || (role.contains("CUSTOMER") && retrievedInteraction.getCustomer().getId() == user_id)){
-            retrievedInteraction.setProduct(productRepository.getById(updatedInteraction.getProduct().getId()));
-            retrievedInteraction.setCustomer(customerRepository.getById(updatedInteraction.getCustomer().getId()));
-            retrievedInteraction.setReview(reviewRepository.getById(updatedInteraction.getReview().getId()));
-            retrievedInteraction.setAmountClicks(updatedInteraction.getAmountClicks());
+        if(retrievedInteraction != null){
+            if(role.contains("ADMIN") || (role.contains("CUSTOMER") && retrievedInteraction.getCustomer().getId() == user_id)){
+                retrievedInteraction.setProduct(productRepository.findProductById(updatedInteraction.getProduct().getId()));
+                retrievedInteraction.setCustomer(customerRepository.findCustomerById(updatedInteraction.getCustomer().getId()));
+                retrievedInteraction.setReview(reviewRepository.findReviewById(updatedInteraction.getReview().getId()));
+                retrievedInteraction.setAmountClicks(updatedInteraction.getAmountClicks());
 
-            interactionRepository.save(retrievedInteraction);
+                interactionRepository.save(retrievedInteraction);
 
-            return ResponseEntity.ok(retrievedInteraction);
-        } else {
-            return new ResponseEntity<String>("Forbidden", HttpStatus.FORBIDDEN);
+                return ResponseEntity.ok(retrievedInteraction);
+            } else {
+                return new ResponseEntity<String>("Not authorized", HttpStatus.FORBIDDEN);
+            }
+        } else{
+            throw new ResourceNotFoundException("Cannot update. Interaction not found for this id: " + updatedInteraction.getId());
         }
+
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteInteraction(@RequestHeader("Authorization") String tokenWithPrefix, @PathVariable long id){
+    public ResponseEntity deleteInteraction(@RequestHeader("Authorization") String tokenWithPrefix, @PathVariable long id) throws ResourceNotFoundException {
         String token = tokenWithPrefix.substring(7);
         Map<String, Object> claims = jwtUtils.extractAllClaims(token);
         String role = claims.get("role").toString();
@@ -130,15 +150,15 @@ public class InteractionController {
                 interactionRepository.delete(interaction);
                 return ResponseEntity.ok().build();
             } else{
-                return ResponseEntity.notFound().build();
+                throw new ResourceNotFoundException("Cannot delete. Interaction not found for this id: " + id);
             }
         } else {
-            return new ResponseEntity<String>("Forbidden", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<String>("Not authorized", HttpStatus.FORBIDDEN);
         }
     }
 
     @PostMapping("/addClick")
-    public ResponseEntity addClick(@RequestHeader("Authorization") String tokenWithPrefix, @RequestBody AddToInteractionDTO addToInteractionDTO){
+    public ResponseEntity addClick(@RequestHeader("Authorization") String tokenWithPrefix, @Valid @RequestBody AddToInteractionDTO addToInteractionDTO){
         String token = tokenWithPrefix.substring(7);
         Map<String, Object> claims = jwtUtils.extractAllClaims(token);
         String role = claims.get("role").toString();
@@ -148,12 +168,12 @@ public class InteractionController {
             interactionService.addClick(addToInteractionDTO);
             return new ResponseEntity<String>("Added", HttpStatus.CREATED);
         } else {
-            return new ResponseEntity<String>("Forbidden", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<String>("Not authorized", HttpStatus.FORBIDDEN);
         }
     }
 
     @PostMapping("/addBuy")
-    public ResponseEntity addBought(@RequestHeader("Authorization") String tokenWithPrefix, @RequestBody AddToInteractionDTO addToInteractionDTO){
+    public ResponseEntity addBought(@RequestHeader("Authorization") String tokenWithPrefix, @Valid @RequestBody AddToInteractionDTO addToInteractionDTO){
         String token = tokenWithPrefix.substring(7);
         Map<String, Object> claims = jwtUtils.extractAllClaims(token);
         String role = claims.get("role").toString();
@@ -163,12 +183,12 @@ public class InteractionController {
             interactionService.addBuy(addToInteractionDTO);
             return new ResponseEntity<String>("Added", HttpStatus.CREATED);
         } else {
-            return new ResponseEntity<String>("Forbidden", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<String>("Not authorized", HttpStatus.FORBIDDEN);
         }
     }
 
     @PostMapping("/addCart")
-    public ResponseEntity addCart(@RequestHeader("Authorization") String tokenWithPrefix, @RequestBody AddToInteractionDTO addToInteractionDTO){
+    public ResponseEntity addCart(@RequestHeader("Authorization") String tokenWithPrefix, @Valid @RequestBody AddToInteractionDTO addToInteractionDTO){
         String token = tokenWithPrefix.substring(7);
         Map<String, Object> claims = jwtUtils.extractAllClaims(token);
         String role = claims.get("role").toString();
@@ -178,7 +198,7 @@ public class InteractionController {
             interactionService.addCart(addToInteractionDTO);
             return new ResponseEntity<String>("Added", HttpStatus.CREATED);
         } else {
-            return new ResponseEntity<String>("Forbidden", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<String>("Not authorized", HttpStatus.FORBIDDEN);
         }
     }
 }
